33287419884e49755be19c66c5321133d850f6d1
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-05-08 08:44:58 +0000

    feat:post、userモデルにアソシエーション、バリデーション、画像アップロードの記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+
+  has_many_attached :images # 複数画像アップロード
+  validate :image_presence # 必ず1枚の画像をアップロードする
+
+  validates :furniture_name, presence: true
+  validates :caption, presence: true, length: { maximum: 2000 }
+
+  private
+
+  def image_presence
+    errors.add(:images, "at least one image is required") unless images.attached?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
+  has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
+
+  has_one_attached :profile_image
+
+  validates :name, length: { maximum: 20 }
+  validates :username, uniqueness: true,
+      length: { minimum: 6, maximum: 20 }
+
+  def get_profile_image(width, height)
+      unless profile_image.attached?
+        file_path = Rails.root.join("app/assets/images/default-image.jpg")
+        profile_image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
+      end
+      profile_image.variant(resize: "#{width}x#{height}^", gravity: "center", crop: "#{width}x#{height}+0+0").processed
+  end
+
+
 end
