e06aefd3541297cb4ac0f93600f2d396a42c2903
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-05-09 11:17:30 +0000

    tailwind切り戻し完了

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+alert('hi')
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
