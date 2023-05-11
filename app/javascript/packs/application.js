2902eff7f9e7498418cb01e0eecae3da3ddbb449
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-05-11 13:07:23 +0000

    feat:FontAwesomeの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "stylesheets/application.scss"
-
+import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
 Turbolinks.start()
