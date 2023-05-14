4bf593048e9c69ee482c54df0d2b0595123c52b5
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-05-14 17:32:36 +0000

    feat: 新規投稿画面のレイアウト

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,6 @@
 class HomesController < ApplicationController
 
   def top
-    @feeds = Post.where(user_id: [current_user.id, *current_user.following_ids]).order(created_at: :desc)
   end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    @feeds = Post.where(user_id: [current_user.id, *current_user.following_ids]).order(created_at: :desc)
   end
 
   def show
