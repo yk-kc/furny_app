2e4c09f0541c1025b3ad92a549341619a037e527
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-05-15 14:01:35 +0000

    feat: いいね非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,22 +3,20 @@ class FavoritesController < ApplicationController
 
   def index
     # ユーザーがいいねした投稿
-    @posts = Post.joins(:favorites).where(favorites: { user_id: params[:user_id] }) 
+    @posts = Post.joins(:favorites).where(favorites: { user_id: params[:user_id] })
   end
 
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: @post.id)
     favorite.save
-    post.create_notification_like!(current_user)
-    redirect_to request.referer
+    @post.create_notification_like!(current_user)
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: @post.id)
     favorite.destroy
-    redirect_to request.referer
   end
 
 end
