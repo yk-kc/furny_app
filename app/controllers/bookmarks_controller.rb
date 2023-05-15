141df46e47222ceda73024ed076e89b0cc68c380
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-05-15 15:51:17 +0000

    feat: ブックマーク非同期化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,17 +7,15 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    post = Post.find(params[:post_id])
-    bookmark = current_user.bookmarks.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    bookmark = current_user.bookmarks.new(post_id: @post.id)
     bookmark.save
-    redirect_to request.referer
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    bookmark = current_user.bookmarks.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    bookmark = current_user.bookmarks.find_by(post_id: @post.id)
     bookmark.destroy
-    redirect_to request.referer
   end
 
 end
