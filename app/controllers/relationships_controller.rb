d9a7ace0b8b82989fffcb1521e1262af5f65ad63
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-05-15 06:39:27 +0000

    fix: relationshipsコントローラ修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,7 +2,7 @@ class RelationshipsController < ApplicationController
 
   # フォローするとき
   def create
-    @user = User.find(params[:relationship][:following_id])
+    @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
     @user.create_notification_follow!(current_user)
     redirect_to request.referer
