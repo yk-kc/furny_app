75f5d51c5b67a67a792fb1a9580ac1c8d29f06fd
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-05-21 23:26:45 +0000

    feat: 通知画面レイアウト

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   after_action :update_notifications, only: [:index]
 
   def index
-    @notifications = current_user.passive_notifications.page(params[:page]).per(20)
+    @notifications = current_user.passive_notifications.page(params[:page]).per(10)
   end
 
   private
