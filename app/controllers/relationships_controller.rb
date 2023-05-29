4f6fb8ed254e350d06a19101ebb5ca0febae3fef
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-05-29 06:49:14 +0000

    fix: レイアウト修正、ページネーション追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -14,12 +14,12 @@ class RelationshipsController < ApplicationController
   # フォロー一覧
   def followings
     user = User.find(params[:user_id])
-    @users = user.followings
+    @users = user.followings.page(params[:page]).per(12)
   end
   # フォロワー一覧
   def followers
     user = User.find(params[:user_id])
-    @users = user.followers
+    @users = user.followers.page(params[:page]).per(12)
   end
 
 end
